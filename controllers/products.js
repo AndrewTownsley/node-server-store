4a8625ba754c886be30b169984dbeffda805b556
Andrew TOwnsley <atownsley86@gmail.com>
2022-01-22 11:20:06 -0700

    Implementing sort feature to sort products.

--- controllers/products.js
+++ controllers/products.js
@@ -2,15 +2,12 @@ const Product = require('../models/product')
 
 
 const getAllProductsStatic = async (req, res) => {
-    const search = 'aaa'
-    const products = await Product.find({ 
-        name: {$regex:search},
-    })
+    const products = await Product.find({}).sort('-name price');
     res.status(200).json({ products, nbHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort } = req.query;
     const queryObject = {}
     
     if(featured) {
@@ -20,11 +17,20 @@ const getAllProducts = async (req, res) => {
         queryObject.company = company;
     }
     if(name) {
-        queryObject.name = name;
+        queryObject.name = { $regex: name, $options: 'i'};
     }
-    console.log(queryObject);
-    const products = await Product.find(queryObject)
-    res.status(200).json({ products, nbHits: products.length})
+    // console.log(queryObject);
+    let result = Product.find(queryObject)
+    if(sort) {
+        console.log(sort);
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    }
+    else {
+        result = result.sort('createdAt');
+    }
+    const products = await result;
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports =  {
